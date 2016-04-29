7cc53fb367f6ac55ec60bbde0ce54a72ae7b95d5
Patrick Gerken <patrick.gerken@zumtobelgroup.com>
2016-04-30 00:27:56 +0200

    Preparing release 0.0.1

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-0.0.1 (unreleased)
+0.0.1 (2016-04-30)
 ------------------
 
 - Initial release.
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os
 def read(*paths):
     return open(os.path.join(os.path.dirname(__file__), *paths), 'r').read()
 
-version = '0.0.1dev0'
+version = '0.0.1'
 
 setup(name='collective.argv0spy',
       version=version,
